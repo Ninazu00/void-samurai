2be1aee04b77f0cd2497fc1ceacf2f3a56c2349c
Ninazu00 <ba.mohammad2005@gmail.com>
2025-12-14 16:36:48 +0200

    Cleaned up scripts

--- void samurai/Assets/Scripts/Yuki.cs
+++ void samurai/Assets/Scripts/Yuki.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Yuki : MonoBehaviour
+public class Yuki : EnemyController
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform target;
 
-    // Update is called once per frame
-    void Update()
+    protected override void  EnemyBehavior()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position,target.transform.position,moveSpeed*Time.deltaTime);
+        return;
     }
 }
